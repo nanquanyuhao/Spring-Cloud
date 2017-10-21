739a3d1d7a5c88e0030d6cec07856aef0f82974b
nanquanyuhao <nanquanyuhao@foxmail.com>
2017-10-22 01:41:21 +0800

    添加忽略文件；添加注册中心集群搭建示例；添加健康检查示例

---
+++ myhealthprovider/src/main/java/net/nanquanyuhao/cloud/MyHealthIndicator.java
@@ -0,0 +1,21 @@
+package net.nanquanyuhao.cloud;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by nanquanyuhao on 2017/10/22.
+ */
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+
+    public Health health() {
+        if (PoliceController.canVisitDb){
+            return new Health.Builder(Status.UP).build();
+        } else {
+            return new Health.Builder(Status.DOWN).build();
+        }
+    }
+}
