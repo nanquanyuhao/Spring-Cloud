57c4e3b496bbd5d4dbfa070d4abf43cf98c0bbf6
nanquanyuhao <nanquanyuhao@foxmail.com>
2017-10-26 16:55:29 +0800

    RestTemplate拦截器的自定义实现及添加示例复习

--- resttpltest/src/main/java/net/nanquanyuhao/cloud/MyConfig.java
+++ resttpltest/src/main/java/net/nanquanyuhao/cloud/MyConfig.java
@@ -18,6 +18,7 @@ import java.util.List;
 @Configuration
 public class MyConfig {
 
+    // 加入自己定义的 负载均衡 注解
     @Autowired(required = false)
     @MyLoadBalanced
     private List<RestTemplate> tpls = Collections.emptyList();
@@ -28,8 +29,10 @@ public class MyConfig {
             public void afterSingletonsInstantiated() {
 
                 for(RestTemplate tpl : tpls){
+                    // debug过程中，拦截器集合中并未预先定义其他拦截器
                     List<ClientHttpRequestInterceptor> interceptors = tpl.getInterceptors();
                     interceptors.add(new MyInterceptor());
+                    // 为唯一的RestTemplate设置拦截器，即将全部请求均转化为对 http://localhost:8080/hello 的访问
                     tpl.setInterceptors(interceptors);
                 }
             }
